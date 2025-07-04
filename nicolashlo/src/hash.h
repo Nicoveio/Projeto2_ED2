f03312658bf12fe4869259b80a0e10fb981a7a4b
Nicoveio <pccontani@gmail.com>
2025-07-04 15:19:32 -0300

    Consertei algumas síntax de funções de busca para retornar um tipo booleano. Inclui a a biblioteca para tipo booleano. Consertei erros de declaração da versão anterior ('typedef* void') para 'typedef void*'. Consertei o parâmetro int da função get para int*. Adicionei os comentários explicativos acima das funções. Sujeito ainda há alteração tais comentários.

--- nicolashlo/src/hash.h
+++ nicolashlo/src/hash.h
@@ -1,14 +1,41 @@
 #ifndef _HASH__
 #define _HASH__
+#include <stdbool.h>
 
-typedef *void hashTable;
+/* 
+Tipo opaco para a Tabela Hash, refere-se a estrutura da tabela. 
+*/
 
-hashTable* createHashTable(int size);
+typedef void* hashTable;
 
+/* 
+Cria uma nova tabela Hash, do tamanho especificado por 'size'.
+Para melhor desempenho, recomenda-se um numero primo maior que o número de elementos esperado.
+*/
+
+hashTable createHashTable(int size);
+
+/* 
+Insere um elemento na tabela através de um par chave-valor, a tabela fará uma cópia interna da chave e atribuirá a um valor.
+Caso o valor associado já exista, seu valor associado é atualizado. 
+*/
 void hashPut(hashTable ht, const char*key, int value);
 
-void hashGet(hashTable ht, const char*key, int value);
+/*
+Busca um elemento na tabela através de sua chave, retornando o estado da busca se bem-sucedida ou mal-sucedida.
+Caso seja bem-sucedida, retorna a 'value' o valor encontrado.
+*/
+
+bool hashGet(hashTable ht, const char*key, int* value);
+
+/*
+Remove um elemento da tabela através de sua chave.
+*/
 
 void hashRemove(hashTable ht, const char* key);
-
+/*
+Destrói a tabela definitivamente.
+*/
 void hashTableDestroy(hashTable ht);
+
+#endif
